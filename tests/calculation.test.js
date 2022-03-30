91aa1cb55e1071284add5f0dddc93960b2cd1255
Hollis Jamison <hollisjamison@gmail.com>
2022-03-29 21:07:25 -0400

    completes the calculation.test.js file

--- tests/calculation.test.js
+++ tests/calculation.test.js
@@ -20,9 +20,24 @@ describe('calculation.js test suite', () => {
 
     it('returns the sum of the numbers in the array provided correctly', () => {
       // Setup mock data to be tested
+      // Setup mock data to be tested
+      const mockData = [10,20,30,40]
+      const expectedResult = 100
+      
+      // Use our mock data in the actual function, and record the result
+      const actualResult = sumAll(mockData)
+      
+      // Compare the result and the proposed result in an assert
+      expect(actualResult).to.be.equal(expectedResult)
     })
 
     it('it works with negative numbers', () => {
+      const mockData = [5,-5,250]
+      const expectedResult = 250
+
+      const actualResult = sumAll(mockData)
+
+      expect(actualResult).to.be.equal(expectedResult)
     })
   })
 })
